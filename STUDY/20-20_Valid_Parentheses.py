a6aa779ffb2b8b2c6f592f381701dc08d75283df
Lena <lny970116@gmail.com>
2022-05-21 11:37:54 +0900

    fix 5/21 study

--- STUDY/20-20_Valid_Parentheses.py
+++ STUDY/20-20_Valid_Parentheses.py
@@ -2,13 +2,17 @@
 # https://leetcode.com/problems/valid-parentheses/7
 
 def isValid(s: str) -> bool:
-    stack = []
-    dic = {'(':')','{':'}','[':']'}
+    li = []
+    dic = {')':'(','}':'{',']':'['} #거꾸로 해야 내가 짠 코드에 들어맞아
 
-    for i in s:
-        if i not in dic:
-            stack.append(i)
-        elif not stack or dic[i] != stack.pop():
+    for i in s: #i = 0번째 인덱스 == '('임 / 그리고 다음은 ')'
+        if i not in dic: #dic 키값과 일치가 x / )는 dic의 키값과 일치해 그래서 elif으로 감
+            li.append(i) #i값 넣어줘
+        elif not li or dic[i] != li.pop():
             return False
-    
-    return len(stack) == 0
+
+    return len(li) == 0
+
+# print(isValid("()[]{}"))
+
+# 책 참고
