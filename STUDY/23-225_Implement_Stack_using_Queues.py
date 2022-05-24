47e21cf1d2c211ec42e969d0ce1d5781553d6a14
Lena <lny970116@gmail.com>
2022-05-25 01:57:51 +0900

    feat: 5/25 study

---
+++ STUDY/23-225_Implement_Stack_using_Queues.py
@@ -0,0 +1,29 @@
+# 48 ms	14 MB
+# https://leetcode.com/problems/implement-stack-using-queues/
+
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        return self.q.pop()
+
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0 # 맞으면 1 아니면 걍 0임
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
