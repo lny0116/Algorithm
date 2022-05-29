e05806200b5f354ac48327cad33cbac977fab4bc
Lena <lny970116@gmail.com>
2022-05-29 21:23:51 +0900

    fix: add comment

--- STUDY/21-316-Remove_Duplicate_Letters.py
+++ STUDY/21-316-Remove_Duplicate_Letters.py
@@ -5,18 +5,22 @@ from collections import Counter
 
 def removeDuplicateLetters(s: str) -> str:
     cnt = Counter(s)
+    # print(cnt) # 1번째= Counter({'b': 2, 'c': 2, 'a': 1})
     seen = set()
+    # print(seen) #set() 출력
     stack = []
 
     for i in s:
-        cnt[i] -= 1
+        cnt[i] -= 1 #카운터 목록에서 하나 제거하고
         if i in seen:
             continue
 
         # 뒤에 붙일 문자가 남아 있다면 스택에서 제거
         while stack and i < stack[-1] and cnt[stack[-1]] > 0:
-            seen.remove(stack.pop())
+            #stack에 있는 문자가 더 크고 cnt가 0보다 크면
+            seen.remove(stack.pop()) #seen과 stack에서 없앰
 
+        #그리고 i를 다시 넣어줌
         stack.append(i)
         seen.add(i)
 
