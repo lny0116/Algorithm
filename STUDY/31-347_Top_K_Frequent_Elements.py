30ec4377827458fb6371ef16d5e8fedf2ee80d77
Lena <lny970116@gmail.com>
2022-06-03 17:58:14 +0900

    feat: 6/3 study

---
+++ STUDY/31-347_Top_K_Frequent_Elements.py
@@ -0,0 +1,34 @@
+#	102 ms	18.7 MB
+# https://leetcode.com/problems/top-k-frequent-elements/
+
+from collections import defaultdict
+
+
+def topKFrequent(nums: list[int], k: int) -> list[int]:
+    dic = defaultdict(int)
+
+    for i in nums:
+        dic[i] += 1
+
+    dic_sort = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+
+    res = []
+
+    for a,b in dic_sort: #a =key, b=value
+        # print(a,b)
+
+        if len(res) < k:
+            res.append(a)
+    
+    return res
+
+
+print(topKFrequent(nums = [1,1,1,2,2,3], k = 2))
+print(topKFrequent(nums = [1], k = 1))
+
+
+'''
+defaultdict(<class 'int'>, {1: 3, 2: 2, 3: 1})
+dict_values([3, 2, 1])
+dict_values([1])
+'''
