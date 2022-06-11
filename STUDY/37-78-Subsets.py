c8ef0d02fcb69a44e3b3657af00f9709246f58b3
Lena <lny970116@gmail.com>
2022-06-11 10:55:22 +0900

    feat: 6/10 study

---
+++ STUDY/37-78-Subsets.py
@@ -0,0 +1,19 @@
+# 68 ms	14.2 MB
+# https://leetcode.com/problems/subsets/
+
+from collections import defaultdict
+
+
+def subsets(nums: list[int]) -> list[list[int]]:
+    res = []
+
+    dfs(nums, [], res)
+    return res
+    
+def dfs(nums, path, res):
+    res.append(path)
+    for i in range(len(nums)): #[1,2,3] 기준 3 == i = 0,1,2
+        dfs(nums[i+1:], path+[nums[i]], res)
+
+print(subsets([1,2,3]))
+print(subsets([0]))
