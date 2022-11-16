b81cb85c80a01603676e84237cf1198f42ca6b11
Lena <lny970116@gmail.com>
2022-11-16 22:21:39 +0900

    Update 자연수 뒤집어 배열로 만들기.js

--- programmers_JS/자연수 뒤집어 배열로 만들기.js
+++ programmers_JS/자연수 뒤집어 배열로 만들기.js
@@ -1,6 +1,21 @@
+// function solution(n) {
+//     var answer = [];
+//     let arr = n.toString().slice(0, n.length);
+    
+//     for (i = 0; i< arr.length; i++){
+//         answer.push(parseInt(arr[i]))
+//     }
+    
+//     answer.reverse();
+    
+//     return answer;
+// }
+
+// let arr = n.toString().slice(0, n.length); -> 이 부분이 서잘데기가 없음...ㅎ
+
 function solution(n) {
     var answer = [];
-    let arr = n.toString().slice(0, n.length);
+    let arr = String(n);
     
     for (i = 0; i< arr.length; i++){
         answer.push(parseInt(arr[i]))
