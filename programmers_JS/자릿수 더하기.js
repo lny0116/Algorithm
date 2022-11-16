ef646b38d4b6831992fdf822f9e91a38bc97e484
Lena <lny970116@gmail.com>
2022-11-16 22:26:31 +0900

    Update 자릿수 더하기.js

--- programmers_JS/자릿수 더하기.js
+++ programmers_JS/자릿수 더하기.js
@@ -1,12 +1,26 @@
+// function solution(n)
+// {
+//     var answer = 0;
+//     let sliceNum = n.toString().slice(0, n.length);
+//     let sum = 0;
+    
+//     for (i = 0; i < sliceNum.length; i++){
+//         sum += Number(sliceNum[i])
+//     }
+
+//     return answer = sum;
+// }
+
+// 서잘데기 없는 것 수정... ㅎㅎ
+
 function solution(n)
 {
     var answer = 0;
-    let sliceNum = n.toString().slice(0, n.length);
-    let sum = 0;
-    
-    for (i = 0; i < sliceNum.length; i++){
-        sum += Number(sliceNum[i])
+    let str = String(n);
+       
+    for (i = 0; i < str.length; i++){
+        answer += Number(str[i])
     }
 
-    return answer = sum;
+    return answer;
 }
