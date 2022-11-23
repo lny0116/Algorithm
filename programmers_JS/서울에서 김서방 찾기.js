ac4fb2fd784ddb81466124b6f579d3d369aad19c
Lena <lny970116@gmail.com>
2022-11-23 20:22:14 +0900

    Create 서울에서 김서방 찾기.js
    https://school.programmers.co.kr/learn/courses/30/lessons/12919

---
+++ programmers_JS/서울에서 김서방 찾기.js
@@ -0,0 +1,11 @@
+function solution(seoul) {
+    let where = [];
+    
+    for (i=0;i<seoul.length;i++){
+        if (seoul[i] === 'Kim'){
+            where.push(i);
+            // console.log(where);
+        }
+    }
+    return answer = `김서방은 ${where[0]}에 있다`;
+}
