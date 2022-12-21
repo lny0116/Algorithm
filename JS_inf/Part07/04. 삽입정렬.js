5bc0786af403f506d84ddfc7781e993692957dc3
Lena <lny970116@gmail.com>
2022-12-21 21:20:12 +0900

    feat: part7

---
+++ JS_inf/Part07/04. 삽입정렬.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    let answer = [];
+    answer.push(arr[0]);
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < answer.length; j++) {
+            if (arr[i] < answer[j]) {
+                answer.splice(j, 0, arr[i]);
+                break;
+            }
+        }
+    }
+    return answer;
+}
+
+let arr = [11, 7, 5, 6, 10, 9];
+console.log(solution(arr));
