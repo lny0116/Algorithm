81e596a6b51015071120507df19f0e4b5771dd3c
Lena <lny970116@gmail.com>
2023-03-25 23:55:06 +0900

    x만큼 간격이 있는 n개의 숫자

--- programmers_JS/x만큼 간격이 있는 n개의 숫자.js
+++ programmers_JS/x만큼 간격이 있는 n개의 숫자.js
@@ -2,7 +2,7 @@ function solution(x, n) {
     var answer = [];
 
     for (let i = 1; i <= n; i++) {
-        answer.push(x*i) //x의 배수
+        answer.push(x * i) //x의 배수
     }
 
     return answer;
